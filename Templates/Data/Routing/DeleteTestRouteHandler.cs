3e3221e873e19e1d4513020d0976f22747f89b3b
Honor <igorpetrov42ru@gmail.com>
2024-03-12 18:29:51 +0300

    -

---
+++ Templates/Data/Routing/DeleteTestRouteHandler.cs
@@ -0,0 +1,14 @@
+using Templates.Data.Configuration;
+
+namespace Templates.Data.Routing;
+
+
+public class DeleteTestRouteHandler : RouteHandler
+{
+    public DeleteTestRouteHandler(RouteConfiguration configuration) : base (configuration) {}
+
+    public override bool Run(ApplicationBuilder builder)
+    {
+        
+    }
+}
