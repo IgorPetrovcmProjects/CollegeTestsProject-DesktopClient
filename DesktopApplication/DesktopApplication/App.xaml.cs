fae1980e3519c2c1f9f8f292bb7639c1e24ff6d3
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-03-15 01:21:11 +0300

    based init desktop application

---
+++ DesktopApplication/DesktopApplication/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace DesktopApplication
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+
+		protected override Window CreateWindow(IActivationState? activationState)
+		{
+			Window window = base.CreateWindow(activationState);
+
+			window.Height = 700;
+			window.Width = 1000;
+			window.MaximumWidth = 1000;
+			window.MinimumWidth = 700;
+			window.MaximumHeight = 700;
+			window.MinimumHeight = 550;
+			return window;
+		}
+	}
+}
---
+++ DesktopApplication/DesktopApplication/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DesktopApplication
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
